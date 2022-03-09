c5424686923e318b9e22558a3c596d1efe4e194c
Vikmah <viktoria.mahrberg@gmail.com>
2022-03-09 11:10:00 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,39 @@
 
 // Vanilla JavaScript Function
 
+        // function addTwoNumbers(a, b) {
+        //    return a + b;
+        //    }
+        //    let sum = addTwoNumbers(1, 3);
+        //    console.log(sum);
+    
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+return a + b;
+}
+let sum = addTwoNumbers(1, 3);
+console.log(sum);
+
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(1, 3);
+console.log(sum2);
+
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `
+    <p>
+    this is a message
+    </p> `
+)
+console.log(returnMultipleLines());
